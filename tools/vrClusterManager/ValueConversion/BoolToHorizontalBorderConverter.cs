71234a40f3942d652aa7f101962dbe00a1f7847a
Andrey Yamashev <ayamashev@gmail.com>
2017-11-05 21:37:57 -0600

    Moved value converters to a separate namespace

---
+++ tools/vrClusterManager/ValueConversion/BoolToHorizontalBorderConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace vrClusterManager.ValueConversion
+{
+	public class BoolToHorizontalBorderConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			bool val = System.Convert.ToBoolean(value);
+			if (val)
+			{
+				return System.Windows.HorizontalAlignment.Left;
+			}
+			else
+			{
+				return System.Windows.HorizontalAlignment.Right;
+			}
+
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
